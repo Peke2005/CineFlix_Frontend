563479f4957686ceddfe33b4da3c975bf00f3cc5
Marc Muntané <mmclara1050@gmail.com>
2025-02-25 16:23:27 +0100

    CineFlix Footer ended

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +7,14 @@ import { Component } from '@angular/core';
   standalone: false,
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'CineFlixFrontend';
+  mostrarFooter: boolean = true;
+
+  constructor(private router: Router) {
+    this.router.events.subscribe(() => {
+      this.mostrarFooter = this.router.url !== '/login'; // Oculta el footer en /login
+    });
+  }
 }
