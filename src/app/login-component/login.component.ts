df2816274d6ad3a818547686c226e454b9b0b024
bryanjrr <bryanjoyarubio@gmail.com>
2025-03-07 16:30:44 +0100

    Creation of Login Component

---
+++ src/app/login-component/login.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'login',
+    templateUrl: './login.component.html',
+    standalone: false,
+    styleUrl: './login.component.css'
+})
+export class loginComponent {
+    llaveIzq = "{";
+    llaveDer = "}";
+}
