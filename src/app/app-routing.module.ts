4a7f41f6c705170f918a12c97a246b170e08c4ad
Marc Muntané <mmclara1050@gmail.com>
2025-03-14 16:37:43 +0100

    Perfil_finalizado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { registerComponent } from './register-component/register.component';
 import { loginComponent } from './login-component/login.component';
+import { profileComponent } from './profile/profile.component'
 
 
 const routes: Routes = [
   { path: 'Register', component: registerComponent },
-  { path: 'Login', component: loginComponent }
+  { path: 'Login', component: loginComponent },
+  { path: 'Profile', component: profileComponent}
 ];
 
 @NgModule({
--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-profile',
+  selector: 'Profile',
   standalone: false, 
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent {}
+export class profileComponent {}
