07752aaf8e38ad031cd527b8c9f942511afe48d4
bryanjrr <bryanjoyarubio@gmail.com>
2025-04-03 17:39:19 +0200

    Componente Film

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,16 +5,17 @@ import { loginComponent } from './login-component/login.component';
 import { profileComponent } from './profile/profile.component';
 import { ComponentHome } from './component-home/component-home';
 import { componentListar } from './component-Listar/component-Listar';
+import { ComponentFilm } from './component-film/component-film';
 
 const routes: Routes = [
   { path: '', component: loginComponent },
   { path: 'Register', component: registerComponent },
+  { path: 'Film', component: ComponentFilm },
   { path: 'Home', component: ComponentHome },
   { path: 'Listar/:genre', component: componentListar },
   { path: 'Login', component: loginComponent },
   { path: 'Profile', component: profileComponent },
   { path: '**', redirectTo: 'Home', pathMatch: 'full' },
-
 ];
 
 @NgModule({
