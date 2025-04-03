d1364b209ca7c9281bb96a70f385fc4273b2ebcd
Peke2005 <polcarbajalgarcia@gmail.com>
2025-04-03 16:30:59 +0200

    Comit acabado pagina main con header

--- src/app/app.service.injectable.ts
+++ src/app/app.service.injectable.ts
@@ -2,12 +2,11 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-
 @Injectable({
   providedIn: 'root',
 })
 export class CineFlixService {
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getAllMovies(): Observable<any> {
     return this.http.get(`/listFilms`);
@@ -17,24 +16,19 @@ export class CineFlixService {
     return this.http.get(`/movieSearchCategory?category=${category}`);
   }
 
+  getMovieByName(name: string): Observable<any> {
+    return this.http.get(`/movieSearchTitle?title=${name}`);
+  }
 
   loginUser(user: any): Observable<any> {
     let body = JSON.stringify(user);
     const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
     return this.http.post<any>('/userLogin', body, { headers: headers });
-
   }
 
   registerUser(user: any): Observable<any> {
     let body = JSON.stringify(user);
     const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
     return this.http.post<any>('/userRegister', body, { headers: headers });
-
   }
-
-  
-
-
-
-
 }
--- src/app/component-header/component-header.ts
+++ src/app/component-header/component-header.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { CineFlixService } from '../app.service.injectable';
 
 @Component({
   selector: 'Component-header',
@@ -8,8 +9,46 @@ import { Router } from '@angular/router';
   styleUrl: './component-header.css',
 })
 export class ComponentHeader {
-  constructor(private Route: Router) {}
+  resultados: any[] = [];
+
+  constructor(
+    private Route: Router,
+    private cineflixservice: CineFlixService
+  ) {}
   searchCategory(genre: String) {
-    this.Route.navigate(['/Listar', genre]);
+    this.Route.navigate(['/Listar/genre', genre]);
+  }
+
+  searchFlimName() {
+    const searchInput = document.getElementById(
+      'nameFilm'
+    ) as HTMLInputElement | null;
+    if (searchInput) {
+      const title = searchInput.value.trim();
+      if (this.resultados != null) {
+        this.resultados = [];
+      }
+
+      this.cineflixservice.getMovieByName(title).subscribe({
+        next: (response) => {
+          console.log(response.data);
+          if (response.data && Array.isArray(response.data)) {
+            this.resultados = response.data.filter((film: any) =>
+              film.title.toUpperCase().includes(title.toUpperCase())
+            );
+            if (this.resultados.length > 0) {
+              console.log('Navigating to:', '/Listar/title', title);
+              this.Route.navigate(['/Listar/title', title]);
+            }
+          } else {
+            this.resultados = [];
+          }
+        },
+        error: (err) => {
+          console.error('Error fetching movies:', err);
+          this.resultados = [];
+        },
+      });
+    }
   }
 }
