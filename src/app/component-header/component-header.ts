bc17de4bbd3a570cf437c01b42958d84b7979f87
Peke2005 <polcarbajalgarcia@gmail.com>
2025-04-04 16:37:04 +0200

    comit modificado css y hecho cosas del header

--- src/app/component-header/component-header.ts
+++ src/app/component-header/component-header.ts
@@ -15,6 +15,11 @@ export class ComponentHeader {
     private Route: Router,
     private cineflixservice: CineFlixService
   ) {}
+
+  navegateToProfile() {
+    this.Route.navigate(['/Profile']);
+  }
+
   searchCategory(genre: String) {
     this.Route.navigate(['/Listar/genre', genre]);
   }
--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'Profile',
-  standalone: false, 
+  standalone: false,
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  styleUrls: ['./profile.component.css'],
 })
-export class profileComponent {}
+export class profileComponent implements OnInit {
+  id: Number | null = 0;
+
+  ngOnInit() {
+    const id = localStorage.getItem('id');
+    if (id) {
+      this.id = Number(id);
+    } else {
+      this.id = 0;
+    }
+  }
+}
